eefe4409266bc1fa1906fd1bdeaef517b84fed5b
Antoine <sid.xxdzs@gmail.com>
2019-11-29 23:26:55 +0100

    Store the projection and height for each point, then draw lines between neighboors

--- src/hooks.c
+++ src/hooks.c
@@ -11,19 +11,24 @@ int fdf_expose(s_window* win)
 	return 0;
 }
 
+# define GET(N) ARRAY_GETL(s_point2_int, screen_points, N)
+
 int fdf_repaint(s_window* win)
 {
 	s_fdf_map* map = (void*)(win + 1);
-	t_array screen_points = fdf_transform(map);
+	t_array screen_points[1] = {fdf_transform(map)};
 
 	ft_printf("Hook %s\n", __FUNCTION__);
-	ft_printf("%lu points\n", screen_points.size);
+	ft_printf("%lu points\n", screen_points->size);
 
-	unsigned i = screen_points.size;
+	unsigned i = screen_points->size;
 	while (i --> 1)
 	{
+		if (i % map->dim.x > 0)
+			draw_line(win, GET(i), GET(i - 1), 255);
+		if (i / map->dim.x > 0)
+			draw_line(win, GET(i), GET(i - map->dim.x), 255);
 	}
-
 	fdf_expose(win);
 	return 0;
 }
--- src/transform.c
+++ src/transform.c
@@ -2,18 +2,23 @@
 
 t_array fdf_transform(s_fdf_map* map)
 {
-	t_array result = NEW_ARRAY(t_3Dpoint);
+	t_array       result = NEW_ARRAY(s_point3_int);
 	s_point3_int* iterator = ARRAY_ITERATOR(&(map->points));
+	t_3Dpoint     point;
+	s_point3_int  screen_point;
 
 	fta_reserve(&result, map->points.size);
 	while ((void*)++iterator < ARRAY_END(&(map->points)))
 	{
-		t_3Dpoint point = MAKE_POINT(float, iterator->x, iterator->y, iterator->z);
+		point = MAKE_POINT(float, iterator->x, -iterator->y, iterator->z);
 
-		iterator->x -= map->dim.x / 2;
-		iterator->y += map->dim.y / 2;
+		point.x -= map->dim.x / 2;
+		point.y += map->dim.y / 2;
 
-		fta_append(&result, &point, 1);
+		/* ... */
+
+		screen_point = MAKE_POINT(int, point.x, point.y, iterator->z);
+		fta_append(&result, &screen_point, 1);
 	}
 	return result;
 }
