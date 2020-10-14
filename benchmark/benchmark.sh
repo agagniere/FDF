8696786b9efa8bdabf5ee132f0b187b219784eb9
Antoine GAGNIERE <sid.xxdzs@yahoo.com>
2020-10-15 01:00:50 +0200

    Benchmark map is now a parameter

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -1,11 +1,17 @@
 # Compile with different flags to compare performance
 
+map="maps/mars.fdf"
+if test -n "$1"
+then
+	map=$1
+fi
+
 make # For submodules
 for OPT in 0 2
 do
 	for TRANSFORM in "-DSIMPLISTIC_TRANSFORM" ""
 	do
 		CPPFLAGS="$TRANSFORM -O$OPT" make re
-		./fdf.exe maps/mars.fdf > mars${OPT}${TRANSFORM}.out
+		./fdf.exe $map >> $(basename ${map%.*})-O${OPT}${TRANSFORM}.out
 	done
 done
