91f8fbe42071375fc250e7b0eb4eedb9168a2a41
Antoine Gagniere <antoine.gagniere@yahoo.com>
2021-05-14 15:38:56 +0200

    Update benchmark.sh

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -5,13 +5,14 @@ if test -n "$1"
 then
 	map=$1
 fi
+name=$(basename ${map%.*})
 
 make # For submodules
-for OPT in 0 2
+for OPT in 0 s 1 2 3 fast
 do
-	for TRANSFORM in "-DSIMPLISTIC_TRANSFORM" ""
+	for TRANSFORM in "" "-DSIMPLISTIC_TRANSFORM"
 	do
 		CPPFLAGS="$TRANSFORM -O$OPT" make re
-		./fdf.exe $map >> $(basename ${map%.*})-O${OPT}${TRANSFORM}.out
+		./fdf.exe $map >> benchmark/${name}-O${OPT}${TRANSFORM}.out
 	done
 done
